d5797cfd49f63408880840efa15e193d433ec3e6
Dejavi77 <javierdelaserna7@gmail.com>
2019-02-06 10:24:41 +0100

    commit 1

--- src/domain/Banks_BDD.py
+++ src/domain/Banks_BDD.py
@@ -6,7 +6,8 @@ def get_bank():
 
 def get_client():
     try:
-        with open('E:\PytonWorkspace\Bank\src\persistence\Clients.txt', 'r') as f:
+        #with open('E:\PytonWorkspace\Bank\src\persistence\Clients.txt', 'r') as f:
+        with open('src/persistence/Clients.txt', 'r') as f:
             line = f.readline()
             return line
     except FileNotFoundError as fnf:
@@ -15,12 +16,14 @@ def get_client():
 
 
 def set_client_info(value):
-    with open('E:\PytonWorkspace\Bank\src\persistence\Clients.txt', 'w') as f:
+    #with open('E:\PytonWorkspace\Bank\src\persistence\Clients.txt', 'w') as f:
+    with open('src/persistence/Clients.txt', 'w') as f:
         f.write(str(value))
         print(str)
 
 
 def save(obj_client):
     print(obj_client.first_name)
-    with open('E:\\PytonWorkspace\\Bank\\src\\persistence\\' + str(obj_client.first_name) + '.txt', 'a') as f:
+    with open('src/persistence/' + str(obj_client.first_name) + '.txt', 'a') as f:
+    #with open('E:\\PytonWorkspace\\Bank\\src\\persistence\\' + str(obj_client.first_name) + '.txt', 'a') as f:
         f.write(repr(obj_client))
